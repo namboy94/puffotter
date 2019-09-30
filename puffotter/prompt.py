66c325e77046298c3fb69246d9fe75c7fcfa687e
Hermann Krumrey <hermann@krumreyh.com>
2019-09-30 20:50:33 +0200

    Add more prompt functions

--- puffotter/prompt.py
+++ puffotter/prompt.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with puffotter.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
-from typing import List
+from typing import List, Any, Callable, Optional
 
 
 def yn_prompt(
@@ -89,3 +89,70 @@ def selection_prompt(objects: List[object]) -> List[object]:
             print("Invalid selection")
         else:
             return selected_objects
+
+
+def prompt_comma_list(
+        message: str,
+        primitive_type: Callable[[str], Any] = str,
+        min_count: int = 0
+) -> List[Any]:
+    """
+    Prompts the user for a comma-separated list
+    :param message: The message to display
+    :param primitive_type: The primitive type of the elements in the list
+    :param min_count: The minimum amount of elements to be provided by the user
+    :return: The result of the prompt
+    """
+    while True:
+        try:
+            response = input(message)
+            result = list(map(
+                lambda x: primitive_type(x.strip()),
+                response.split(",")
+            ))
+
+            if len(result) < min_count:
+                print("Not enough values")
+                raise ValueError()
+            else:
+                return result
+
+        except (ValueError, TypeError):
+            print("Invalid input")
+
+
+def prompt(
+        prompt_text: str = "",
+        default: Optional[Any] = None,
+        _type: Callable[[str], Any] = str,
+        required: bool = True
+) -> Optional[Any]:
+    """
+    Generic prompt with configuration options
+    :param prompt_text: The text to display before the prompt
+    :param default: A default value to use if the user responds with ''
+    :param _type: The type of the object prompted. Must take a single string
+                  as a parameter
+    :param required: Whether or not as response is required
+    :return: The prompt result. May be None if required is False
+    """
+    if default is not None:
+        prompt_text += " {}".format(str(default))
+    prompt_text += ":"
+
+    response = input(prompt_text).strip()
+    while response == "" and default is None:
+        response = input(prompt_text).strip()
+
+    if response == "" and default is not None:
+        return default
+    elif response == "":
+        if required:
+            return prompt(prompt_text, default, _type, required)
+        else:
+            return None
+    else:
+        try:
+            return _type(response)
+        except (TypeError, ValueError):
+            return prompt(prompt_text, default, _type, required)
